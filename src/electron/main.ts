167713dd4a931fb7f9245969b30f71b5e0387e71
unknown <Anthony>
2025-01-25 01:33:26 +0800

    window-commit-resolve-error

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -5,14 +5,12 @@ import { getPreloadPath, getUIPath } from "./pathResolver.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
-    show : false,
     webPreferences : {
       preload : getPreloadPath(),
     }
   });
 
   mainWindow.maximize();
-  mainWindow.show()
 
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123");
